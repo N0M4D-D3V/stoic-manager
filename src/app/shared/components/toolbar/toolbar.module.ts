8c9a0f6013ade549d15ae1d24566644d954a624e
N0M4D <victor.develops@gmail.com>
2023-10-30 13:01:31 +0100

    toolbar | toggleMenu first implementation

---
+++ src/app/shared/components/toolbar/toolbar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ToolbarComponent } from './toolbar.component';
+
+@NgModule({
+  declarations: [ToolbarComponent],
+  exports: [ToolbarComponent],
+  imports: [],
+  providers: [],
+  bootstrap: [],
+})
+export class ToolbarModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ToolbarModule } from './components/toolbar/toolbar.module';
+
+@NgModule({
+  declarations: [],
+  exports: [ToolbarModule],
+  imports: [ToolbarModule],
+  providers: [],
+  bootstrap: [],
+})
+export class SharedModule {}
